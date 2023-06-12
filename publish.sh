3b82d39a47cc7568be1ad996b9fe7673506c1ab4
Vincent Yuan <vincentyuan1218@126.com>
2023-06-11 20:14:45 -0500

    Update site

--- publish.sh
+++ publish.sh
@@ -2,10 +2,12 @@ rm -rf static
 rm -rf docs
 
 
-bash get_web_page.txt
+# bash get_web_page.txt
 
-# gssg --productionDomain https://realvincentyuan.github.io/Spacecraft
+gssg --productionDomain https://realvincentyuan.github.io/Spacecraft
 
+
+# gssg --url https://realvincentyuan.github.io/Spacecraft  --dest https://github.com/Realvincentyuan/Spacecraft
 
 mv static docs
 
