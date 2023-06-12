8affa712db64edbe1f6b58f6861ba32a731a13b4
Vincent Yuan <vincentyuan1218@126.com>
2023-06-11 20:22:56 -0500

    restore part of the site

--- publish.sh
+++ publish.sh
@@ -2,12 +2,10 @@ rm -rf static
 rm -rf docs
 
 
-# bash get_web_page.txt
+bash get_web_page.txt
 
-gssg --productionDomain https://realvincentyuan.github.io/Spacecraft
+# gssg --productionDomain https://realvincentyuan.github.io/Spacecraft
 
-
-# gssg --url https://realvincentyuan.github.io/Spacecraft  --dest https://github.com/Realvincentyuan/Spacecraft
 
 mv static docs
 
